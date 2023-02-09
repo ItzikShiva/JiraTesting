0b88873f47a1bde02737b0564d6de8c5c198e58a
Itzik <itzikv3@gmail.com>
2023-02-09 09:57:30 +0200

    create issue 9-2-23

--- src/test/java/jira/api/issue/CreateIssueTests.java
+++ src/test/java/jira/api/issue/CreateIssueTests.java
@@ -1,5 +1,6 @@
 package jira.api.issue;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -36,7 +37,7 @@ public class CreateIssueTests extends BaseIssueTests {
         logger.info("got create issue Metadata");
 
         CreateIssueRequest createIssueRequest = new CreateIssueRequest();
-        insertValuesForCreateIssue(createIssueRequest);
+        insertValuesForCreateIssue(createIssueRequest, true);
         response = issueService.createIssue(createIssueRequest);
         Assert.assertEquals(response.code(), 201);
         CreateIssueResponse createIssueResponse = responseToObject(response, CreateIssueResponse.class);
@@ -54,11 +55,51 @@ public class CreateIssueTests extends BaseIssueTests {
         logger.info("issue with key: " + createdIssueKey + " Deleted successfully from server");
     }
 
+    @Test
+    public static void createIssueWithMissingFields() {
+        apiService.login();
+        // we not really use Metadata(next 2 lines) in that flow, just assert that we get it.
+        Response response = issueService.getCreateMetadata();
+        Assert.assertEquals(response.code(), 200);
+        logger.info("got create issue Metadata");
 
-    public static void insertValuesForCreateIssue(CreateIssueRequest createIssueRequest) {
+        CreateIssueRequest createIssueRequest = new CreateIssueRequest();
+        response = issueService.createIssue(createIssueRequest);
+        Assert.assertEquals(response.code(), 400);
+        logger.info("Issue was not created - problem in JSON request");
+    }
+
+    @Test
+    public static void createIssueWithInvalidFieldValues() {
+        apiService.login();
+        // we not really use Metadata(next 2 lines) in that flow, just assert that we get it.
+        Response response = issueService.getCreateMetadata();
+        Assert.assertEquals(response.code(), 200);
+        logger.info("got create issue Metadata");
+
+        CreateIssueRequest createIssueRequest = new CreateIssueRequest();
+        insertValuesForCreateIssue(createIssueRequest, false);
+        response = issueService.createIssue(createIssueRequest);
+        Assert.assertEquals(response.code(), 400);
+        //TODO - ask, Hod there are different errors, depend which value is missing, so i print the obj response, ok?
+        try {
+            logger.info("Issue was not created - problem in: " + response.body().string());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * this method insert values for CreateIssueRequest,the second parameter - true for valid values, false for invalid values
+     */
+    public static void insertValuesForCreateIssue(CreateIssueRequest createIssueRequest, boolean validValues) {
         Fields fields = new Fields();
         fields.setSummary(SUMMARY);
-        fields.setIssuetype(new Issuetype(ISSUE_TYPE));
+        if (validValues) {
+            fields.setIssuetype(new Issuetype(ISSUE_TYPE));
+        } else {
+            fields.setIssuetype(new Issuetype("10070"));
+        }
         fields.setProject(new Project(PROJECT_ID));
         fields.setCustomfield_10020(CUSTOM_FIELD_10020_ID);
         fields.setReporter(new Reporter(REPORTER_ID));
